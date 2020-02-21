9917083b030cd19a497d9c5422b0ea0e75315ea9
shomurodov <59568887+shomurodov@users.noreply.github.com>
2020-02-21 16:58:00 +0500

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,29 @@
+function plus(){
+	var a = document.getElementById("ac").value;
+		a = parseInt(a)
+	var b = document.getElementById("bc").value;
+		b = parseInt(b)
+	var c =document.getElementById("q").innerHTML = a+b;
+}
+function minus(){
+	var a = document.getElementById("ac").value;
+		a = parseInt(a)
+	var b = document.getElementById("bc").value;
+		b = parseInt(b)
+	var c =document.getElementById("q").innerHTML = a-b;
+}
+function um(){
+	var a = document.getElementById("ac").value;
+		a = parseInt(a)
+	var b = document.getElementById("bc").value;
+		b = parseInt(b)
+	var c =document.getElementById("q").innerHTML = a*b;
+}
+function de(){
+	var a = document.getElementById("ac").value;
+		a = parseInt(a)
+	var b = document.getElementById("bc").value;
+		b = parseInt(b)
+	var c =document.getElementById("q").innerHTML = a/b;
+}
+
